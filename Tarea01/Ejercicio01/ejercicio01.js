230d5ffef8f41fe41fdb6fb6be21964b7156ffc6
Donato <j.donato.m@gmail.com>
2026-03-04 17:58:58 -0600

    Archivos Tarea 01

---
+++ Tarea01/Ejercicio01/ejercicio01.js
@@ -0,0 +1,4 @@
+const number = 24
+const result = (number >= 10 && number <= 20) || (number >= 40 && number <= 60);
+
+console.log("El cálculo de rangos para el número", number, "es:", result);
---
+++ Tarea01/Ejercicio02/ejercicio02.js
@@ -0,0 +1,4 @@
+const dogAge = 5
+const humanAge = Math.min(dogAge, 2) * 10 + Math.max(0, dogAge -2) * 4
+
+console.log("El perrito de", dogAge, "años tendría", humanAge, "años si fuera humano.");
