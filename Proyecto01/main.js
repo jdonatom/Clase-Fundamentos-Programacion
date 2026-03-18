04c62e99f6c45e40ee96dc7396af1acc721a6108
Donato <j.donato.m@gmail.com>
2026-03-18 14:16:12 -0600

    Corrección01

--- Proyecto01/main.js
+++ Proyecto01/main.js
@@ -13,36 +13,36 @@ console.log ('El precio de su tiquete es: 0');
 }
 
 // mayor de 65 sin cupon
-if (age >= 65 && hasCoupon == false){
+else if (age >= 65 && hasCoupon == false){
 console.log ('El precio de su tiquete es:', ticketPrice - ((ticketPrice / 100)*40));
 }
 
 // mayor de 65 con cupon
-if (age >= 65 && hasCoupon == true && doubleDiscount65 == true){
+else if (age >= 65 && hasCoupon == true && doubleDiscount65 == true){
 console.log ('El precio de su tiquete es:', ticketPrice - ((ticketPrice / 100)*couponDiscount));
 }
 
 // entre 13 y 64
-if (age > 12 && age < 65 && hasStudentId == false && hasCoupon == false){
+else if (age > 12 && age < 65 && hasStudentId == false && hasCoupon == false){
 console.log ('El precio de su tiquete es:', ticketPrice);
 }
 
 // entre 13 y 64 + estudiante sin cupon
-if (age > 12 && age < 65 && hasStudentId == true && hasCoupon == false){
+else if (age > 12 && age < 65 && hasStudentId == true && hasCoupon == false){
 console.log ('El precio de su tiquete es:', ticketPrice - ((ticketPrice / 100)*25));
 }
 
 // entre 13 y 64 + cupon sin ser estudiante
-if (age > 12 && age < 65 && hasCoupon == true && hasStudentId == false){
+else if (age > 12 && age < 65 && hasCoupon == true && hasStudentId == false){
 console.log ('El precio de su tiquete es:', ticketPrice - ((ticketPrice / 100)*couponDiscount));
 }
 
 // entre 12 y 64 + estudiante + cupon>25
-if (age > 12 && age < 65 && hasStudentId == true && hasCoupon == true && doubleDiscount == true){
+else if (age > 12 && age < 65 && hasStudentId == true && hasCoupon == true && doubleDiscount == true){
 console.log ('El precio de su tiquete es:', ticketPrice - ((ticketPrice / 100)*couponDiscount));
 } 
 
 // entre 12 y 64 + estudiante + cupon<25
-if (age > 12 && age < 65 && hasStudentId == true && hasCoupon == true && doubleDiscount == false){
+else if (age > 12 && age < 65 && hasStudentId == true && hasCoupon == true && doubleDiscount == false){
 console.log ('El precio de su tiquete es:', ticketPrice - ((ticketPrice / 100)*25));
 }
